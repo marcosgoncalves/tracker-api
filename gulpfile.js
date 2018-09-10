f0d115ee695451d2238dd3285e89e0a08bc6595f
Marcos <marcosgoncalves@gmail.com>
2018-09-10 01:18:48 -0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// Installs the required missing modules
+/* */
+(function () {
+	var r = require;
+	require = function (n) {
+		try {
+			return r(n);
+		} catch (err) {
+			// var cmd = 'npm install ' + n + ' --only=dev';
+			var cmd = 'npm install --save-dev ' + n;
+			console.log('\n');
+			console.log('** Required module "' + n + '" not found. Trying to install it **\n');
+			console.log('running: ' + cmd + '\n');
+			r('child_process').execSync(cmd, {
+				stdio: [0, 1, 2]
+			});
+			console.log('\n');
+			return r(n);
+		}
+	};
+})();
+/* */
+
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	watch = require('gulp-watch');
+
+gulp.task('logar', function () {
+	console.log('logar');
+});
+
+gulp.task('serve', function () {
+	connect.server({
+		root: 'app',
+		host: '0.0.0.0',
+		port: 42125
+	});
+});
+
+gulp.task('wcomps', function () {
+	return watch('../comps/src/**', { ignoreInitial: false })
+		.pipe(gulp.dest('app/assets-bower/comps/src'));
+});
+
+//gulp.task('default', gulp.series('serve'));
