290653cbce8b2d146b6684bd5098e851e2e616c0
Marcos <marcosgoncalves@gmail.com>
2018-09-10 02:09:22 -0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,26 @@
+// Installs the required missing modules
+/* */
+(function () {
+	var r = require;
+	require = function (n) {
+		try {
+			return r(n);
+		} catch (err) {
+			// var cmd = 'npm install ' + n + ' --only=dev';
+			var cmd = 'npm install --save-dev ' + n;
+			console.log('\n');
+			console.log('** Required module "' + n + '" not found. Trying to install it **\n');
+			console.log('running: ' + cmd + '\n');
+			r('child_process').execSync(cmd, {
+				stdio: [0, 1, 2]
+			});
+			console.log('\n');
+			return r(n);
+		}
+	};
+})();
+/* */
+
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	watch = require('gulp-watch');
